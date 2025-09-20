95f9079eb13b7192368c009f7d0a6a6e95ccc59f
HaoLi980405 <hl1390208045@gmail.com>
2025-09-20 21:34:08 +0800

    [bugfix]gsa fix running reqs exceed 30 bug (#195)
    * increase q cache num
    
    * deal 抢占bug
    
    * ci
    
    ---------
    
    Co-authored-by: xujia <42216276@qq.com>
    Co-authored-by: zbb200819 <1130072360@qq.com>

--- ucm/ucm_sparse/gsa.py
+++ ucm/ucm_sparse/gsa.py
@@ -125,6 +125,8 @@ class GSAReqStat:
             True if self.num_prompt_tokens > SEG_PREFILL_THRESHOLD else False
         )
         self._init_slot(offset)
+        if len(self.repre_slot_mapping) > len(self.blocks):
+            self.repre_slot_mapping = self.repre_slot_mapping[: len(self.blocks)]
 
     def updata_req_state(
         self, num_scheduled_tokens, add_req_state, index_in_batch
@@ -147,6 +149,8 @@ class GSAReqStat:
             else:
                 self.calc_block_table = []
                 self.calc_repre_slot_mapping = []
+        if len(self.repre_slot_mapping) > len(self.blocks):
+            self.repre_slot_mapping = self.repre_slot_mapping[: len(self.blocks)]
 
     def _get_sparse_and_free_block(self):
         if self.num_prompt_tokens == self.num_computed_tokens:
@@ -400,7 +404,7 @@ class GSA(UcmSparseBase):
         max_model_len = vllm_config.model_config.max_model_len
         max_num_seqs = vllm_config.scheduler_config.max_num_seqs
         self.gsa_offload_ops = gsa_offload_ops.CalKpreAndTopk(
-            self.layer_num, block_size, MAX_BS, att_num_heads, head_size
+            self.layer_num, block_size, max_num_seqs, att_num_heads, head_size
         )
         self.gsa_offload_ops.set_kpre_method_param(
             int(max_model_len / block_size) * MAX_BS, kv_num_heads, 1
